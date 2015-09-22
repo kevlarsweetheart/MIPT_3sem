1a3d58b0223e0f610f1fbc74449701254824b6d4
Maria Efremova <megamafania@land.ru>
2015-09-22 13:19:42 +0300

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -3,15 +3,16 @@
 #include <stdlib.h>
 
 const char *delimiters = ",. ?!():-/;#$%& \t\n";
+const int max_length = 1000000;
 
-void Split(char *string, char *delimiters, char ***tokens, int *tokensCount)
+void split(char *string, char *delimiters, char ***tokens, int *tokens_count)
 {
 	char *token;
 	int tokens_size = 0, next_size = 4, cnt = 0;
 	char **buff_tokens = *tokens;
 	for (token = strtok(string, delimiters); token != NULL; token = strtok(NULL, delimiters))
 	{
-		if (*tokensCount == tokens_size)
+		if (*tokens_count == tokens_size)
 		{
 			tokens_size = next_size;
 			next_size *= 2;
@@ -20,20 +21,22 @@ void Split(char *string, char *delimiters, char ***tokens, int *tokensCount)
 		buff_tokens[cnt++] = token;
 	}
 	*tokens = buff_tokens;
-	*tokensCount = cnt;
+	*tokens_count = cnt;
 }
 
 int main()
 {
-	char *str = (char *)malloc(sizeof(char));
+	char *str = (char *)malloc(max_length * sizeof(char));
 	gets(str);
 	char **tokens = NULL;
-	int tokensCount = 0;
-	Split(str, delimiters, &tokens, &tokensCount);
+	int tokens_count = 0;
+	split(str, delimiters, &tokens, &tokens_count);
 	int i;
-	printf("The string contains %d tokens\n", tokensCount);
+	printf("The string contains %d tokens\n", tokens_count);
 	printf("The tokens:\n");
-	for (i = 0; i < tokensCount; ++i)
+	for (i = 0; i < tokens_count; ++i)
 		printf("%s\n", tokens[i]);
+	free(str);
+	free(tokens);
 	return 0;
 }
