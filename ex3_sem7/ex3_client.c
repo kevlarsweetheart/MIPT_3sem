9cced22dd6441ee595135c87b864b8b9fff7d3d7
madplaguerat <megamafania@land.ru>
2015-10-26 13:04:15 +0300

    seminar 7 exercise 3

---
+++ ex3_sem7/ex3_client.c
@@ -0,0 +1,55 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	
+	char path_name[] = "ex3_client.c";
+	struct mymsgbuf
+	{
+		long mtype;
+		pid_t pid;
+		int x, y;
+	} req_mes;
+	printf("Please, enter your request\n");
+	scanf("%d%d", &req_mes.x, &req_mes.y);
+	req_mes.pid = getpid();
+	req_mes.mtype = 1;
+
+	struct mymsgbuf2
+	{
+		long mtype;
+		int res;
+	} outdata;
+
+	key_t key = ftok(path_name, 0);
+	if (key < 0)
+	{
+		printf("Can't generate key\n");
+		exit(-1);
+	}
+	int msqid = msgget(key, 0666 | IPC_CREAT);
+	if (msqid < 0)
+	{
+		printf("Can't get msqid\n");
+		exit(-1);
+	}
+	if (msgsnd(msqid, (struct msgbuf *) &req_mes, sizeof req_mes - sizeof(long), 0) < 0)
+	{
+		printf("Can't send message to queue\n");
+		msgctl(msqid, IPC_RMID, (struct msqid_ds *)NULL);
+		exit(-1);
+	}
+
+	if (msgrcv(msqid, (struct msgbuf *) &outdata, sizeof outdata - sizeof(long), req_mes.pid, 0) < 0)
+	{
+		printf("Can't receive a message from queue\n");
+		exit(-1);
+	}
+	printf("Multiplication result: %d\n", outdata.res);
+	return 0;
+}
---
+++ ex3_sem7/ex3_server.c
@@ -0,0 +1,105 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sem.h>
+
+const int max_clients = 2;
+
+struct thread_data
+{
+	int msqid, sem_id, x, y;
+	pid_t pid;
+};
+
+int init_queue(key_t key)
+{
+	int msqid = msgget(key, 0666 | IPC_CREAT);
+	if (msqid < 0)
+	{
+		printf("Can't get message queue id T_T\n");
+		exit(-1);
+	}
+	return msqid;
+}
+
+int init_semaphore(key_t key)
+{
+	int sem_id = semget(key, 1, 0666 | IPC_CREAT);
+	if (sem_id < 0)
+	{
+		printf("Can't get semaphore id >_<\n");
+		exit(-1);
+	}
+	return sem_id;
+}
+
+void change_sem(int sem_id, int val)
+{
+	struct sembuf my_buf;
+	my_buf.sem_op = val;
+	my_buf.sem_num = 0;
+	my_buf.sem_flg = 0;
+	semop(sem_id, &my_buf, 1);
+}
+
+void *create_thread(void *dummy)
+{
+
+	struct thread_data data;
+	data = *(struct thread_data *)dummy;
+	change_sem(data.sem_id, -1);
+	struct mymsgbuf2
+	{
+		long mtype;
+		int res;
+	} outdata;
+	outdata.mtype = data.pid;
+	outdata.res = data.x * data.y;
+	if (msgsnd(data.msqid, (struct msgbuf *) &outdata, sizeof outdata - sizeof(long), 0) < 0)
+	{
+		printf("Can't send a message to queue *sad girl smile*\n");
+		msgctl(data.msqid, IPC_RMID, (struct msqid_ds *)NULL);
+		exit(-1);
+	}
+	change_sem(data.sem_id, 1);
+}
+
+int main()
+{
+	char pathname[] = "ex3_client.c";
+	key_t key = ftok(pathname, 0);
+	if (key < 0)
+	{
+		printf("Can't generate key :'(\n");
+		exit(-1);
+	}
+
+	struct thread_data data;
+	data.msqid = init_queue(key);
+	data.sem_id = init_semaphore(key);
+	struct mymsgbuf
+	{
+		long mtype;
+		pid_t pid;;
+		int x, y;
+	} req_mes;
+
+	change_sem(data.sem_id, max_clients);
+
+	while (1)
+	{
+		if(msgrcv(data.msqid, (struct msgbuf *) &req_mes, sizeof req_mes - sizeof(long), 1, 0) < 0)
+		{
+			printf("Can't recieve a message from queu =>_<=\n");
+			exit(-1);
+		}
+		data.pid = req_mes.pid;
+		data.x = req_mes.x;
+		data.y = req_mes.y;
+		pthread_t thread;
+		int exec_res = pthread_create(&thread, (pthread_attr_t *)NULL, create_thread, &data);
+	}
+	return 0;
+}
