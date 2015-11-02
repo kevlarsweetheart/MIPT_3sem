8e37c2b46eae8722459809d757daa769bd008e15
madplaguerat <megamafania@land.ru>
2015-11-02 14:26:26 +0300

    update 02.11.2015

--- ex3_sem7/ex3_client.c
+++ ex3_sem7/ex3_client.c
@@ -4,34 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-/*
- * Чтобы не дублировать дважды объявление структур для пересылки, их можно вынести в отдельный 
- * файл mytypes.h
- * В коде подключить этот заголовочный файл: #include "mytypes.h"
- * Потом при компиляции его дополнительно указывать и для клиента, и для сервера:
- * gcc client.c mytypes.h -o client
- */
+#include "my_types.h"
 
 int main()
 {
+	
 	char path_name[] = "ex3_client.c";
-	struct mymsgbuf
-	{
-		long mtype;
-		pid_t pid;
-		int x, y;
-	} req_mes;
+	
+	struct mymsgbuf req_mes;
 	printf("Please, enter your request\n");
 	scanf("%d%d", &req_mes.x, &req_mes.y);
 	req_mes.pid = getpid();
 	req_mes.mtype = 1;
 
-	struct mymsgbuf2
-	{
-		long mtype;
-		int res;
-	} outdata;
+	struct mymsgbuf2 outdata;
 
 	key_t key = ftok(path_name, 0);
 	if (key < 0)
