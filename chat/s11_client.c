ec6b3a4c33cc54790d80dc53e307c5c5fa3d14f0
madplaguerat <megamafania@land.ru>
2015-12-01 18:16:09 +0300

    upgrade of the chat

--- chat/s11_client.c
+++ chat/s11_client.c
@@ -7,88 +7,85 @@
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 const int max_msg_len = 1024;
 const int port = 51000;
 
-void *recieve(void *dummy)
-{
-	sleep(1);
-	int sockfd = *(int *)dummy;
-	char *msg = (char *)malloc(max_msg_len * sizeof(char));
-	while (1) {
-		bzero(msg, sizeof msg);
-		int err_flag =
-		    recvfrom(sockfd, msg, max_msg_len, 0,
-			     (struct sockaddr *)NULL, NULL);
-		if (err_flag < 0) {
-			perror(NULL);
-			close(sockfd);
-			exit(1);
-		}
-		printf("%s", msg);
-	}
-}
-
 int main(int argc, char **argv)
 {
-	int sockfd;
-	int n, len;
-	char sendline[max_msg_len];
-	struct sockaddr_in servaddr, cliaddr;
+    int sockfd;
+    int n, len;
+    char sendline[max_msg_len];
+    struct sockaddr_in servaddr, cliaddr;
 
-	if (argc != 2) {
-		printf("Usage: ./a.out <IP address>\n");
-		exit(1);
-	}
-	if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror(NULL);
-		exit(1);
-	}
+    if (argc != 2) {
+	printf("Usage: ./a.out <IP address>\n");
+	exit(1);
+    }
+    if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+	perror(NULL);
+	exit(1);
+    }
 
-	bzero(&cliaddr, sizeof(cliaddr));
-	cliaddr.sin_family = AF_INET;
-	cliaddr.sin_port = htons(0);
-	cliaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bzero(&cliaddr, sizeof(cliaddr));
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_port = htons(0);
+    cliaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	if (bind(sockfd, (struct sockaddr *)&cliaddr, sizeof(cliaddr)) < 0) {
-		perror(NULL);
-		close(sockfd);
-		exit(1);
-	}
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(port);
-	if (inet_aton(argv[1], &servaddr.sin_addr) == 0) {
-		printf("Invalid IP address\n");
-		close(sockfd);
-		exit(1);
-	}
+    if (bind(sockfd, (struct sockaddr *) &cliaddr, sizeof(cliaddr)) < 0) {
+	perror(NULL);
+	close(sockfd);
+	exit(1);
+    }
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    if (inet_aton(argv[1], &servaddr.sin_addr) == 0) {
+	printf("Invalid IP address\n");
+	close(sockfd);
+	exit(1);
+    }
 
-	pthread_t thread;
-	pthread_create(&thread, (pthread_attr_t *) NULL, recieve, &sockfd);
-
-	printf("Type your name below\n");
+    int proc = fork();
+    if (proc > 0) {
+	printf
+	    ("Type your name below. Remember that it should start with '#'-symbol\n");
 	scanf("%s", &sendline);
-	int err_flag =
-	    sendto(sockfd, sendline, strlen(sendline) + 1, 0,
-		   (struct sockaddr *)&servaddr, sizeof(servaddr));
+	int err_flag = sendto(sockfd, sendline, strlen(sendline) + 1, 0,
+			      (struct sockaddr *) &servaddr,
+			      sizeof(servaddr));
 	if (err_flag < 0) {
-		perror(NULL);
-		close(sockfd);
-		exit(1);
+	    perror(NULL);
+	    close(sockfd);
+	    exit(1);
 	}
 	while (1) {
-		fgets(sendline, max_msg_len, stdin);
-		err_flag =
-		    sendto(sockfd, sendline, strlen(sendline) + 1, 0,
-			   (struct sockaddr *)&servaddr, sizeof servaddr);
-		if (err_flag < 0) {
-			perror(NULL);
-			close(sockfd);
-			exit(1);
-		}
+	    fgets(sendline, max_msg_len, stdin);
+	    err_flag =
+		sendto(sockfd, sendline, strlen(sendline) + 1, 0,
+		       (struct sockaddr *) &servaddr, sizeof servaddr);
+	    if (err_flag < 0) {
+		perror(NULL);
+		close(sockfd);
+		exit(1);
+	    }
 	}
-	return 0;
+    }
+
+    if (proc == 0) {
+	char *msg = (char *) malloc(max_msg_len * sizeof(char));
+	while (1) {
+	    bzero(msg, sizeof msg);
+	    int err_flag =
+		recvfrom(sockfd, msg, max_msg_len, 0,
+			 (struct sockaddr *) NULL, NULL);
+	    if (err_flag < 0) {
+		perror(NULL);
+		close(sockfd);
+		exit(1);
+	    }
+	    printf("%s", msg);
+	}
+    }
+    return 0;
 }
--- chat/s11_server.c
+++ chat/s11_server.c
@@ -8,98 +8,97 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-//Местами очень странное форматирование - результат работы indent -linux
-
 const int max_msg_len = 1024;
 const int port = 51000;
+const int max_clients = 10;
 
 struct client {
-	struct sockaddr_in client_data;
-	char *client_name;
+    struct sockaddr_in client_data;
+    char *client_name;
 };
 
 char *form_msg(char *name, char *msg)
 {
-	char *formed = (char *)malloc(max_msg_len * sizeof(char));
-	strcpy(formed, name);
-	strcat(formed, ": ");
-	strcat(formed, msg);
-	return formed;
+    char *formed = (char *) malloc(max_msg_len * sizeof(char));
+    strcpy(formed, name);
+    strcat(formed, ": ");
+    strcat(formed, msg);
+    return formed;
 }
 
-char *sender_search(struct sockaddr_in sender, struct client *users, int *num,
-		    char *name)
+int main()
 {
-	int ind = 0;
-	while (ind < *num && !
-	       (users[ind].client_data.sin_addr.s_addr == sender.sin_addr.s_addr
-		&& users[ind].client_data.sin_port == sender.sin_port))
-		ind++;
-	if (ind == *num) {
-		users =
-		    (struct client *)realloc(users,
-					     (ind +
-					      1) * sizeof(struct client));
-		users[ind].client_name =
-		    (char *)malloc(strlen(name) * sizeof(char) + 1);
-		strcpy(users[ind].client_name, name);
-		users[ind].client_data = sender;
-		*num = ind + 1;
-		return name;
-	} else {
-		return users[ind - 1].client_name;
+    int sockfd;
+    int clilen, n;
+    char *line = (char *) malloc(max_msg_len * sizeof(char));
+    struct sockaddr_in servaddr, cliaddr;
+    int num = 0;
+    struct client *users =
+	(struct client *) malloc(max_clients * sizeof(struct client));
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+	perror(NULL);
+	exit(1);
+    } else {
+	printf("Sock fd:%d\n", sockfd);
+    }
+
+    if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+	perror(NULL);
+	close(sockfd);
+	exit(1);
+    }
+
+    while (1) {
+	bzero(line, sizeof line);
+	clilen = sizeof cliaddr;
+	if ((n = recvfrom(sockfd, line, max_msg_len - 1, 0,
+			  (struct sockaddr *) &cliaddr, &clilen)) < 0) {
+	    perror(NULL);
+	    close(sockfd);
+	    exit(1);
 	}
-}
-
-int main(){
-	int sockfd;
-	int clilen, n;
-	char *line = (char *)malloc(max_msg_len * sizeof(char));
-	struct sockaddr_in servaddr, cliaddr;
-	int num = 0;
-	struct client *users = NULL;
-
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(port);
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror(NULL);
-		exit(1);
-	} else {
-		printf("Sock fd:%d\n", sockfd);
-	}
-
-	if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-		perror(NULL);
-		close(sockfd);
-		exit(1);
-	}
-
-	while (1) {
-		bzero(line, sizeof line);
-		clilen = sizeof cliaddr;
-		if ((n = recvfrom(sockfd, line, max_msg_len - 1, 0,
-				  (struct sockaddr *)&cliaddr, &clilen)) < 0) {
-			perror(NULL);
-			close(sockfd);
-			exit(1);
+	//printf("Addr: %d\n", cliaddr.sin_addr.s_addr);
+	
+	if (line[0] != '#') {
+	    int i = 0;
+	    for (i = 0; i < num; ++i) {
+		if (users[i].client_data.sin_addr.s_addr ==
+		    cliaddr.sin_addr.s_addr
+		    && users[i].client_data.sin_port == cliaddr.sin_port)
+		    break;
+	    }
+	    strcpy(line, form_msg(users[i].client_name, line));
+	    //printf("i = %d\n", i);
+	    //printf("%s\n", line);
+	    for (i = 0; i < num; ++i) {
+		if (sendto
+		    (sockfd, line, strlen(line), 0,
+		     (struct sockaddr *) &(users[i].client_data),
+		     clilen) < 0) {
+		    perror(NULL);
+		    close(sockfd);
+		    exit(1);
 		}
-
-		cliaddr.sin_port = port + 1;
-		char *user = sender_search(cliaddr, users, &num, line);
-		line = form_msg(user, line);
-		int i;
-		for (i = 0; i < num; ++i) {
-			if (sendto(sockfd, line, strlen(line), 0,
-				   (struct sockaddr *)&(users[i].client_data),
-				   clilen) < 0) {
-				perror(NULL);
-				close(sockfd);
-				exit(1);
-			}
-		}
+	    }
+	} else {
+	    users[num].client_name = (char *)malloc(max_msg_len * sizeof(char));
+	    strcat(users[num].client_name, line);
+	    users[num].client_data.sin_addr.s_addr = cliaddr.sin_addr.s_addr;
+	    users[num].client_data.sin_port = cliaddr.sin_port;;
+	    num++;
+	    printf("Freshman in the thread!\n");
 	}
-	return 0;
+	/*printf("num = %d ", num);
+	int counter = 0;
+	for(counter; counter < num; counter++)
+		printf("%s:%d:%d\t", users[counter].client_name, users[counter].client_data.sin_addr.s_addr, users[counter].client_data.sin_port);
+	printf("\n");*/
+    }
+    return 0;
 }
